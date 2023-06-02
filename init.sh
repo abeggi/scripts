24cc914abaab596112015e461839a7f6ce8f57fc
abeggi <49241476+abeggi@users.noreply.github.com>
2023-06-02 10:51:27 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ apt install mc htop ncdu -y
 wget https://github.com/abeggi/scripts/raw/main/.bash_aliases
 wget https://github.com/abeggi/scripts/raw/main/aggiorna.sh
 chmod +x aggiorna.sh
+timedatectl set-timezone Europe/Rome
