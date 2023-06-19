f5dc544171facc8292fd370a4bc4607127b278be
abeggi <49241476+abeggi@users.noreply.github.com>
2023-06-19 13:51:43 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #
 apt update
-apt install mc htop ncdu -y
+apt install mc htop ncdu tldr -y
 wget https://github.com/abeggi/scripts/raw/main/.bash_aliases
 wget https://github.com/abeggi/scripts/raw/main/aggiorna.sh
 chmod +x aggiorna.sh
