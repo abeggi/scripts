9e6ed9165c18ae5d67bb95d4c3347ab717de1481
abeggi <49241476+abeggi@users.noreply.github.com>
2023-06-22 17:01:43 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 #
+cd
+sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc
 apt update
-apt install mc htop ncdu tldr -y
+apt install curl wget mc htop ncdu tldr -y
 wget https://github.com/abeggi/scripts/raw/main/.bash_aliases
 wget https://github.com/abeggi/scripts/raw/main/aggiorna.sh
 chmod +x aggiorna.sh
