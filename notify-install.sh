771e5377cff202133f0acaa07042b3890069dfce
abeggi <49241476+abeggi@users.noreply.github.com>
2024-10-13 20:17:49 +0200

    Update notify-install.sh

--- notify-install.sh
+++ notify-install.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 cd /usr/local/bin
+rm /usr/local/bin/notify.sh
 wget https://github.com/abeggi/scripts/raw/main/notify.sh
 chmod +x notify.sh
 cd /etc/systemd/system/
+systemctl disable notify.service
+rm /etc/systemd/system/notify.service
 wget https://github.com/abeggi/scripts/raw/main/notify.service
 systemctl enable notify.service
 systemctl start notify.service
