a1eed2da6efa28fc6afd41176e49fdb2ad388377
abeggi <49241476+abeggi@users.noreply.github.com>
2024-10-13 20:02:28 +0200

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,3 +1,4 @@
-#!/bin/bash hostname=$(hostname)
+#!/bin/bash
+hostname=$(hostname)
 ip_address=$(ip -4 addr show dev wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1)
 curl -d "Hostname: $hostname, Interface: wlan0, IP: $ip_address" ntfy.sh/beggi-pi
