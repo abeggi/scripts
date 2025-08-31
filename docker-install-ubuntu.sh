2d7549e70fb697d8f242d195af34e7db715c01fd
abeggi <49241476+abeggi@users.noreply.github.com>
2025-08-31 08:59:10 +0200

    Update docker-install-ubuntu.sh

--- docker-install-ubuntu.sh
+++ docker-install-ubuntu.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
-apt-get install ca-certificates curl gnupg -y
-mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get install ca-certificates gnupg apt-transport-https curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt-get install docker-ce
