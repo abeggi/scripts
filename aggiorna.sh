051382364b27d68d5b5b5b17b34f3af073461628
abeggi <49241476+abeggi@users.noreply.github.com>
2026-01-19 20:43:52 +0100

    Update aggiorna.sh

--- aggiorna.sh
+++ aggiorna.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-#
-apt update
-apt upgrade -y
-apt autoremove --purge -y
-apt autoclean -y
+set -e
+
+# Assicura che apt non chieda input
+export DEBIAN_FRONTEND=noninteractive
+
+# Aggiornamento completo e pulizia
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove --purge -y
+apt-get autoclean -y
+rm -rf /var/lib/apt/lists/*
