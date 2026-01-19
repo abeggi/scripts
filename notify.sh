cd2d24851e76918b2bc5298197aed8e3a72a67e3
abeggi <49241476+abeggi@users.noreply.github.com>
2026-01-19 15:36:10 +0100

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 hostname=$(hostname)
 ip_address=$(ip -4 addr show dev wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1)
-curl -H "Tags: green_circle" -H "Title: $hostname: " -d "$ip_address" ntfy.sh/beggi-pi
+curl -H "Tags: green_circle" -H "Title: $hostname: " -d "$ip_address" ntfy.sh/beggi-pi2
